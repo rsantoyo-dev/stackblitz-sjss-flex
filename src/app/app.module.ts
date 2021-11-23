67c54aa656ab7b5c94f68b5a0071d1d4b6d72419
rsantoyo-dev <ricardo.santoyo@hotmail.com>
2021-11-23 07:32:51 -0500

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SuperJssModule } from 'super-jss';
-import { BoxComponent } from './box/box.component';
 import { PaperComponent } from './paper/paper.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, SuperJssModule],
-  declarations: [AppComponent, HelloComponent, BoxComponent, PaperComponent],
+  declarations: [AppComponent, HelloComponent, PaperComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
