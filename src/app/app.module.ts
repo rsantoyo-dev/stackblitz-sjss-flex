6c7f45fa6ce4f502568bd0334203e6c2243b395d
rsantoyo-dev <ricardo.santoyo@hotmail.com>
2021-11-30 11:46:51 -0500

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { SuperJssModule } from 'super-jss';
 import { PaperComponent } from './paper/paper.component';
 import { SelectorComponent } from './selector/selector.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, SuperJssModule],
-  declarations: [AppComponent, HelloComponent, PaperComponent, SelectorComponent],
+  declarations: [AppComponent, PaperComponent, SelectorComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
